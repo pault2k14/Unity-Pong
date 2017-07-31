ff6574ca671b3923e407456e900a49803f450ede
Paul Thompson <pbt@pdx.edu>
2017-07-31 01:30:46 -0700

    first commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float speed = 30;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+        float racketHeight)
+    {
+        // ascii art:
+        // ||  1 <- at the top of the racket
+        // ||
+        // ||  0 <- at the middle of the racket
+        // ||
+        // || -1 <- at the bottom of the racket
+
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Note: 'col' holds the collision information. If the
+        // Ball collided with a racket, then:
+        //   col.gameObject is the racket
+        //   col.transform.position is the racket's position
+        //   col.collider is the racket's collider
+
+        if(collision.gameObject.name == "RacketLeft")
+        {
+            // Calculate hit factor
+            float y = hitFactor(transform.position,
+                collision.transform.position,
+                collision.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via.normalized
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        if (collision.gameObject.name == "RacketRight")
+        {
+            // Calculate hit factor
+            float y = hitFactor(transform.position,
+                collision.transform.position,
+                collision.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via.normalized
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+
+    }
+
+}
